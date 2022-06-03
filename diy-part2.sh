bb94e5ae06466e9836f5d408e56812a9963d8a98
oceanuse <31952574+oceanuse@users.noreply.github.com>
2022-06-03 21:48:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@
 sed -i 's/192.168.1.1/192.168.204.70/g' package/base-files/files/bin/config_generate
 
 # 2.清除默认密码/改密码为 
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 3.修改默认主题
 # sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
